f0a06e1305c7c20bafc98a605c7c54752eb9dc3b
codingrev <emmowu10@gmail.com>
2022-01-23 13:44:31 +0000

    added-js-for-integration
    -

---
+++ connect-mail-api.js
@@ -0,0 +1,24 @@
+submitContactFormThroughApi(){
+	body={
+		"company_name":document.querySelector("#mailCompanyName").value,
+		"company_email":document.querySelector("#mailCompanyEmail").value,
+		"name":document.querySelector("#mailName").value,
+		"email":document.querySelector("#mailEmail").value,
+		"message":document.querySelector("#mailMessage").value,
+	};
+	const headers = {
+		"Content-Type": "application/json",
+		"Accept": "application/json",
+	};
+	fetch("https://manuel-mailapi.herokuapp.com/api/", {
+			method: "POST",
+			headers,
+			body: JSON.stringify(body),
+	})
+	.then((response){
+		alert("Sent")
+	})
+	.catch((e){
+		alert("Couldn't send mail.")
+	})
+};
